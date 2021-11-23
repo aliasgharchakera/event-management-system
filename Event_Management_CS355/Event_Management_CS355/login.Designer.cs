a41537ab271cf17bbfe7fe38c5b45b780098962e
UzairAmin101 <44573912+UzairAmin101@users.noreply.github.com>
2021-11-23 14:35:33 +0500

    login updated

--- Event_Management_CS355/Event_Management_CS355/login.Designer.cs
+++ Event_Management_CS355/Event_Management_CS355/login.Designer.cs
@@ -35,25 +35,26 @@ namespace Event_Management_CS355
             this.password_textbox = new System.Windows.Forms.TextBox();
             this.login_label = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // email_label
             // 
             this.email_label.AutoSize = true;
-            this.email_label.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.email_label.Location = new System.Drawing.Point(91, 148);
             this.email_label.Name = "email_label";
-            this.email_label.Size = new System.Drawing.Size(98, 13);
+            this.email_label.Size = new System.Drawing.Size(94, 13);
             this.email_label.TabIndex = 0;
             this.email_label.Text = "Email / Student ID";
             // 
             // password_label
             // 
             this.password_label.AutoSize = true;
-            this.password_label.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.password_label.Location = new System.Drawing.Point(133, 172);
             this.password_label.Name = "password_label";
-            this.password_label.Size = new System.Drawing.Size(56, 13);
+            this.password_label.Size = new System.Drawing.Size(53, 13);
             this.password_label.TabIndex = 1;
             this.password_label.Text = "Password";
             // 
@@ -63,6 +64,7 @@ namespace Event_Management_CS355
             this.email_textbox.Name = "email_textbox";
             this.email_textbox.Size = new System.Drawing.Size(166, 20);
             this.email_textbox.TabIndex = 2;
+            this.email_textbox.TextChanged += new System.EventHandler(this.email_textbox_TextChanged);
             // 
             // password_textbox
             // 
@@ -77,27 +79,38 @@ namespace Event_Management_CS355
             // 
             this.login_label.AutoSize = true;
             this.login_label.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.login_label.Font = new System.Drawing.Font("Roboto", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.login_label.Location = new System.Drawing.Point(53, 33);
             this.login_label.Name = "login_label";
-            this.login_label.Size = new System.Drawing.Size(194, 77);
+            this.login_label.Size = new System.Drawing.Size(195, 73);
             this.login_label.TabIndex = 4;
             this.login_label.Text = "Login";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(244, 227);
+            this.button1.Location = new System.Drawing.Point(372, 234);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(291, 234);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Login_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(491, 309);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.login_label);
             this.Controls.Add(this.password_textbox);
@@ -120,6 +133,7 @@ namespace Event_Management_CS355
         private System.Windows.Forms.TextBox password_textbox;
         private System.Windows.Forms.Label login_label;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
